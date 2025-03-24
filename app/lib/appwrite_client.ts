88a10df2335189a83ae42bc6b03539ac92425a8a
shruti <shrutim@softype.com>
2025-03-24 17:43:07 +0530

    thrid commit

--- app/lib/appwrite_client.ts
+++ app/lib/appwrite_client.ts
@@ -2,7 +2,7 @@ import { Client } from "appwrite";
 import { env } from "process";
 
 const client = new Client()
-    .setProject(process.env.NEXT_PROJECT_APPWRITE_PROJECT_ID as string);
-
+    .setProject(process.env.NEXT_PROJECT_APPWRITE_PROJECT_ID as string)
+    .setEndpoint(env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string);
 
 export default client;
